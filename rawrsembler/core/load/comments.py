510d911bb21909e9e60cdf191e2c0759548eeaa2
Lord225 <zlotymaciej@gmail.com>
2025-01-30 22:37:27 +0100

    comments with ;

--- rawrsembler/core/load/comments.py
+++ rawrsembler/core/load/comments.py
@@ -6,7 +6,7 @@ import core.config as config
 
 def remove_comments(lines: List[Line], context: Context):
     for line in lines:
-        if "//" in line.line:
+        if "//" in line.line or ";" in line.line:
             # Check if it's inside a string
             if line.line.find('"') < line.line.find("//") and line.line.find('"') > 0:
                 continue
